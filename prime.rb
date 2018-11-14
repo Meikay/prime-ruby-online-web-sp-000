d52957c1af403cecc086391997dd1e4cd9fdc3f0
Meisuleen Kased <meisuleen@gmail.com>
2018-11-14 02:40:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,18 @@
-# require 'pry'
-# def prime?(int)
-#
-# end
-def prime? (n)
-    if n <= 1
-        false
-    elsif n == 2
-        true
-    else
-        (2..n/2).none? { |i| n % i == 0}
+require 'pry'
+def prime?(int)
+  (2..number-1).each do |num|
+    if int%num == 0
+      return false 
     end
+  end
+  return true   
 end
+# def prime? (n)
+#     if n <= 1
+#         false
+#     elsif n == 2
+#         true
+#     else
+#         (2..n/2).none? { |i| n % i == 0}
+#     end
+# end
