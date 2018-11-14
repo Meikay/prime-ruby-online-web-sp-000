9a2b13c0344a23a9701b1f0553235908bc2c5f76
Meisuleen Kased <meisuleen@gmail.com>
2018-11-14 02:31:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # require 'pry'
 # def prime?(int)
-#   
+#
 # end
 def prime? (n)
     if n <= 1
         false
     elsif n == 2
         true
-    else 
+    else
         (2..n/2).none? { |i| n % i == 0}
     end
 end
